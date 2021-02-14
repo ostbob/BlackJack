6b40510b38e3cae975dc5423b79bdf3167bde77e
ostbobmuon <ostbobmuon@gmail.com>
2021-02-14 17:20:10 +0300

    Deck class added

---
+++ Deck.rb
@@ -0,0 +1,16 @@
+class Deck
+  attr_accessor :cards
+
+  def initialize()
+    faces = *(2..10) + ['Jack','Queen','King','Ace']
+    suits = ['+','<3','^','<>']
+    
+    @faces.each do |face|
+      @suits.each do |suit|
+        @cards << Card.new(face, suit)
+      end
+    end
+
+    @cards.shuffle!
+  end
+end
